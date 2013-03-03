b54db833f7a83e63943d04299935272979108b13
niila <peatypurple@gmail.com>
2013-03-03 12:10:21 +0100

    add info window

--- app/models/scotch.rb
+++ app/models/scotch.rb
@@ -4,4 +4,11 @@ class Scotch < ActiveRecord::Base
 	after_validation :geocode
 	acts_as_gmappable :process_geocoding => false
 	attr_accessible :area, :address, :taste, :name #:picture
+
+  def gmaps4rails_infowindow
+  # add here whatever html content you desire, it will be displayed when users clicks on the marker
+  "<b> #{name}</b>, #{area} <br/> #{taste}"
+  end
 end
+
+
